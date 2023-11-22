9fb53c50d56ee339450e3425c5b29bafbc1d6b25
20shobert <150851206+20shobert@users.noreply.github.com>
2023-11-22 15:26:01 -0700

    Went home

---
+++ la12/resources/animate.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    //$('.sue').slideUp(1000);
+    //$('.sue').slideDown(1000);
+
+    $('div').fadeOut(1000);
+    $('div').fadeIn(1000);
+
+    $('#fireworks').click(function() {
+        alert("hi");
+    })
+
+})
