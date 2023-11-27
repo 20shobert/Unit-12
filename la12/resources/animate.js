66b4c1484083a06b4fbeba19996685c263f68b05
20shobert <150851206+20shobert@users.noreply.github.com>
2023-11-27 13:24:26 -0700

    Finished la12

--- la12/resources/animate.js
+++ la12/resources/animate.js
@@ -1,13 +1,73 @@
+// class = .class
+// id = #id
+
+
 $(document).ready(function() {
 
-    //$('.sue').slideUp(1000);
-    //$('.sue').slideDown(1000);
-
-    $('div').fadeOut(1000);
-    $('div').fadeIn(1000);
+    /*$('.sue').slideUp(1000);
+    $('.sue').delay(1000).slideDown(1000, function() {
+        $('div').fadeOut(2000);
+        $('div').delay(1000).fadeIn(2000);
+    });*/
 
     $('#fireworks').click(function() {
-        alert("hi");
-    })
+        $('h2').fadeOut();
+
+        $('.purple').animate({ //we're resetting the animation so it can be played again
+            height: '0px',
+            width: '0px',
+            top: '300px',
+            left: '460px'
+        }, function() {
+            $(this).show();
+        });
+
+        $('.green').animate({ //we're resetting the animation so it can be played again
+            height: '0px',
+            width: '0px',
+            top: '300px',
+            left: '460px'
+        }, function() {
+            $(this).show();
+        });
+
+        $('.red').animate({ //we're resetting the animation so it can be played again
+            height: '0px',
+            width: '0px',
+            top: '300px',
+            left: '460px'
+        }, function() {
+            $(this).show();
+        });
+
+        $('.purple').animate({ // effect, timer, linear or swing, and what to do once done
+            height: '640px',
+            width: '640px',
+            top: '15px',
+            left: '150px'
+        }, 600 /*timer*/, 'linear' /*easing*/, function() { /*callback*/
+            $(this).hide(); // you can use 'this' inside the callback
+        });
+
+        $('.green').delay(560).animate({
+            height: '640px',
+            width: '640px',
+            top: '15px',
+            left: '150px'
+        }, 600 /*timer*/, 'linear' /*easing*/, function() { /*callback*/
+            $(this).hide();
+        });
+
+        $('.red').delay(1100).animate({
+            height: '640px',
+            width: '640px',
+            top: '15px',
+            left: '150px'
+        }, 600 /*timer*/, 'linear' /*easing*/, function() { /*callback*/
+            $(this).fadeOut(300);
+        });
+
+        $('h2').delay(1500).fadeIn();
+    });
 
 })
